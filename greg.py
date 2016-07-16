8800b14633085d0bf81fce3ff0153f2f5ec25dc4
gregorylburgess <gregorylburgess@gmail.com>
2016-07-15 14:05:10 -1000

    Modified the handling of SeqIO objects.  Fixed logic in error matching.

--- greg.py
+++ greg.py
@@ -11,15 +11,14 @@ import subprocess
 def parseALN(filePath):
 	handle = open(filePath, "rU")
 	records = SeqIO.parse(handle, "clustal")
-	handle.close()
 	return records
 
 
 
 # filePath: "" path to fasta file
-# return {id:seq}
-def parseFASTA(filePath, nameFn):
-	records = SeqIO.index(filePath, "fasta")
+# return { x.id : x.seq } A SeqIO index object (non iterable)
+def indexFASTA(filePath, nameFn=lambda x : x.split('|')[0]):
+	records = SeqIO.index(filePath, "fasta", key_function=nameFn)
 	return records
 
 
@@ -27,46 +26,57 @@ def parseFASTA(filePath, nameFn):
 # fastaFilePath: "" path to fasta file
 # targetIDs: 	 [] list of IDs to pull
 #
-# returns: 	 list of SeqIO records
+# returns: 	 [SeqIO.record, ...] record.id and record.seq
 def searchFastaByID(fastaFilePath, targetIDs):
 	records = []
-	db = parseFASTA(fastaFilePath)
-	for record in targetIDs:
-	    if record in db.keys():
-		records.append(db[record])
+	db = indexFASTA(fastaFilePath)
+	for record in targetIDs:#TODO split record names on |, take [0]
+		# Find the sequence if it exists
+		try:
+			records.append(db[record])
+		# Don't care if its not in the DataBase	
+		except:
+			pass
 	return records
 
 
 
 # fastaFile: ""	The fastafile to align.
 # takes in fasta file, does alignment, and outputs fastFile.aln
+# returns the filepath of the resulting alignment file (clustW's .aln format)
 def clustalAlign(fastaFile):
 	print("Calling clustalW...")
 	outFileName = fastaFile + ".aln"
 	subprocess.call(["clustalw", fastaFile, "-outfile=" + outFileName ,"-quiet"])
+	return outFileName
 
 
 # line: "" 	A sequence to edit
+# return ""	The string with inner gaps replaced by ':'
 def replaceInnerGap(line):
-	firstLetter = re.search(r'^-*[^-]', line).start()
-	lastLetter = re.search(r'[^-]$', line).end()
-	line = line[ :firstLetter] + line[firstLetter:lastLetter].replace('-', ':') + line[lastLetter: ]
-	return line
+	frstA = re.search(r'^-*[^-]', line).end() - 1
+	lastA = re.search(r'[^-]-*$', line).start()
+	return line[ :frstA] + (line[frstA:lastA].replace('-', ':')) + line[lastA: ]
 
 
 # aligns [] 	A list of aligned sequences
 # returns 	The mismatch % between aligned sequences
-def computeDist_outterGapConserved(aligns):
+def computeDist_outterGapConserved(alignFile):
 	miss = 0
-	total = len(aligns[0])
-	for line in aligns:
-		replaceInnerGap(line)
+	succ = 0
+	aligns = [replaceInnerGap(str(x.seq)) for x in parseALN(alignFile)]
 	for col in izip(*aligns):
 		colSet = set(col)
+		print(colSet)
 		setSize = len(colSet)
-		if (setSize > 2) or (setSize == 2 and '-' in colSet):
+		if ':' in colSet or (setSize > 2) or (setSize == 2 and not '-' in colSet):
+			print ("M")
 			miss = miss + 1
-	return miss/total
+		else:
+			succ = succ + 1
+	print ("Miss=" + str(miss))
+	print("Hit="+str(succ))
+	return miss / (miss + float(succ))
 
 
 # alginFile: "" An .aln file from clustalw
@@ -79,39 +89,39 @@ def computeDist(alignFile, distFn):
 	else:
 		raise NameError("Error: Invalid grading Fn.  Valid options are:" + distFns.keys())
 
-# t {{}{}} 	A tree from loadTree()
-# s {{}{}} 	A tree form loadSequences()
-# tax a;b;c;d 	A semicolon-delimited taxonomy string
-# upToLvl #	A digit between 1 and 8 indicating depth of tax
-# refDB "" 	File path to the fasta DB
-# outFile ""	Output name for FASTA of matching Taxonomic entries and their sequences
-# distFn ""	Uppercase ID for the dist function to use
-def computeConservPct(t, s, tax, upToLvl, refDB, outFile, distFn):
+# t {{}{}} 		A tree from loadTree()
+# s {{}{}} 		A tree form loadSequences()
+# tax a;b;c;d 		A semicolon-delimited taxonomy string
+# upToLvl #		A digit between 1 and 8 indicating depth of tax
+# refDB "" 		File path to the fasta DB
+# hitsFastaFilePath ""	Output name for FASTA of matching Taxonomic entries and their sequences
+# distFn ""		Uppercase ID for the dist function to use
+def computeConservPct(t, s, tax, upToLvl, refDB, hitsFastaFilePath, distFn):
 	child_seqs = getSequencesOf(t, s, tax, upToLvl)
-	print (child_seqs)
+	print list(chain.from_iterable(child_seqs))
 	print("Searching Fasta...")
 	children = searchFastaByID(refDB, chain.from_iterable(child_seqs))
-	SeqIO.write(children, outFile, "fasta")
-	clustalAlign(outFile)
-	pct = computeDist(outFile, distFn)
+	SeqIO.write(children, hitsFastaFilePath, "fasta")
+	alignFile = clustalAlign(hitsFastaFilePath)
+	pct = computeDist(alignFile, distFn)
 	return pct
 
 
 #Load Tree and Sequences
-
+'''
 t=loadTree("data/Unique_taxonomy.lines")
 s=loadSequences("data/seq_lin.mapping")
-fastaFilePath="data/bold_coi_11_05_2015.fasta"
+DB="data/bold_unique.fasta"
 outFile="temp.fasta"
 tax="Animalia;Arthropoda;Remipedia;Nectiopoda"
 upToLvl=8
-computeConservPct(t, s, tax, upToLvl, fastaFilePath, outFile, "OUTTER_GAP_CONSERVED")
-
-
-
-
-
-
-
+distFn="OUTTER_GAP_CONSERVED"
+computeConservPct(t, s, tax, upToLvl, DB, outFile, distFn)
+'''
+hitsFastaFilePath="data/test.fasta"
+distFn="OUTTER_GAP_CONSERVED"
+alignFile = "data/test.fasta.aln"#clustalAlign(hitsFastaFilePath)
+pct = computeDist(alignFile, distFn)
+print(pct)
 
 
